1065756527257ca4e5959b8dfcd4ad1fb8685dc0
TonyNashed <70116928+TonyNashed@users.noreply.github.com>
2026-08-06 11:07:55 +0300

    Remove unnecessary whitespace in app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const testRoutes = require("./routes/testRoutes");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use("/test", testRoutes);
-
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
-
 module.exports = app;
